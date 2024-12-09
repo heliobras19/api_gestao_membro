ee0a958594597ac61ffb24ce4a50236d7d1896cc
Helio <heliobras3@hotmail.com>
2024-12-09 17:53:04 +0100

    filtrar por tipo

--- app/Http/Controllers/GestaoQuotasController.php
+++ app/Http/Controllers/GestaoQuotasController.php
@@ -11,10 +11,18 @@ class GestaoQuotasController extends Controller
     public function index(Request $request) {
         try {
             $request->validate([
-            'id' => 'required'
+            'id' => 'required',
+            'tipo' => 'required'
             ]);
             
-            $membro = Membro::with('orgaos', 'funcoes')->find($request->id);
+            $membro = Membro::with([
+                'orgaos' => function ($query) use ($request) {
+                    $query->where('tipo', $request->tipo);
+                },
+                'funcoes' => function ($query) use ($request){
+                    $query->where('tipo', $request->tipo);
+                },
+            ])->find($request->id);
             return response()->json(["membro" => $membro]);
         } catch (\Throwable $th) {
             return response()->json(["success" => false, "msg" => $th->getMessage()], 400);
@@ -32,6 +40,7 @@ class GestaoQuotasController extends Controller
                 if(!empty($request->post('orgaos')))  $membro->orgaos()->sync($request->post('orgaos'));
                 if(!empty($request->post('funcoes'))) $membro->funcoes()->sync($request->post('funcoes'));
                 DB::commit();
+                $membro->load('orgaos', 'funcoes');
                 return response()->json(["success" => true, "membro" => $membro]);
             } catch (\Throwable $th) {
                return response(["success" => false, "msg" => $th->getMessage()]);
